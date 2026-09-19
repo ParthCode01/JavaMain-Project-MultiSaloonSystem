9c1ae8de240f72ccd09cabe619a5bb756184da46
ParthCode01 <parthcsework@gmail.com>
2026-09-20 01:20:20 +0530

    Clean up deployment debugging

--- src/main/java/com/parth/saloonmanagement/security/JwtAuthenticationFilter.java
+++ src/main/java/com/parth/saloonmanagement/security/JwtAuthenticationFilter.java
@@ -49,9 +49,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
             if (jwtService.isTokenValid(jwt, userDetails)) {
 
-                System.out.println("AUTH USER: " + userDetails.getUsername());
-                System.out.println("AUTH ROLES: " + userDetails.getAuthorities());
-
                 UsernamePasswordAuthenticationToken authentication =
                         new UsernamePasswordAuthenticationToken(
                                 userDetails,
